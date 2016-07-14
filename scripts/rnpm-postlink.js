c5522cfcf03bb289ffc5e80ebbd5ea9a881b1bb0
liubinbin <lotosbin@gmail.com>
2016-07-14 17:55:50 +0800

    fix postlink

--- scripts/rnpm-postlink.js
+++ scripts/rnpm-postlink.js
@@ -1,41 +1,41 @@
 #!/usr/bin/env node
 
 'use strict';
-
-var fs = require('fs');
+var log = require('npmlog');
+// var fs = require('fs');
 var rnpm = require('rnpm/src/config');
-var xcode = require('xcode');
+// var xcode = require('xcode');
 
-// The current working directory should be project root of the app that is linking react-native-sqlite.
+log.heading = 'rnpm-link react-native-rongcloud-imlib';
+// // The current working directory should be project root of the app that is linking react-native-sqlite.
 var config = rnpm.getProjectConfig();
-
 if (config.ios) {
-    var pbxproj = config.ios.pbxprojPath;
-    var project = xcode.project(pbxproj).parseSync();
-    var target = project.getFirstTarget().uuid;
+    log.info('ios')
+//     var pbxproj = config.ios.pbxprojPath;
+//     var project = xcode.project(pbxproj).parseSync();
+//     var target = project.getFirstTarget().uuid;
+//     // // Create a Frameworks group if necessary.
+//     // if (!project.pbxGroupByName('Frameworks')) {
+//     //     var group = project.pbxCreateGroup('Frameworks', '""');
+//     //     var mainGroup = project.getFirstProject().firstProject.mainGroup;
 
-    // // Create a Frameworks group if necessary.
-    // if (!project.pbxGroupByName('Frameworks')) {
-    //     var group = project.pbxCreateGroup('Frameworks', '""');
-    //     var mainGroup = project.getFirstProject().firstProject.mainGroup;
+//     //     project.getPBXGroupByKey(mainGroup).children.push({
+//     //         value: group,
+//     //         comment: 'Frameworks',
+//     //     });
+//     // }
 
-    //     project.getPBXGroupByKey(mainGroup).children.push({
-    //         value: group,
-    //         comment: 'Frameworks',
-    //     });
-    // }
+//     // ['libsqlite3.0'].forEach(function(name) {
+//     //     project.addFramework('usr/lib/' + name + '.tbd', {
+//     //         lastKnownFileType: 'sourcecode.text-based-dylib-definition',
+//     //         sourceTree: 'SDKROOT',
+//     //         target: target,
+//     //     });
+//     // });
 
-    // ['libsqlite3.0'].forEach(function(name) {
-    //     project.addFramework('usr/lib/' + name + '.tbd', {
-    //         lastKnownFileType: 'sourcecode.text-based-dylib-definition',
-    //         sourceTree: 'SDKROOT',
-    //         target: target,
-    //     });
-    // });
-
-    // fs.writeFileSync(pbxproj, project.writeSync());
-    console.log('rnpm postlink ios')
+//     // fs.writeFileSync(pbxproj, project.writeSync());
+//     log.info('rnpm postlink ios')
 }
 if (config.android) {
-    console.log('rnpm postlink android')
+    log.info('android')
 }
